2dc6724cc633da9dec07e770726666c78d876df2
Ingo Hohmann <mail1@2b1.de>
2019-09-19 20:10:18 +0200

    editor: cleanup and event handling

--- editor.r
+++ editor.r
@@ -1,10 +1,10 @@
 REBOL [
-   Title: "WASM Console Editor"
+   Title: "Web Console Editor"
    Author: "Ingo Hohmann"
 ]
 
 remove-editor: js-native []{
-   var rightPane = document.getElementById( "right")
+   var rightPane = document.getElementById( "rightPane")
    if (rightPane) {
       rightPane.parentNode.removeChild( rightPane)
    }
@@ -18,10 +18,10 @@ add-editor: js-native[]{
       body {
          display: flex;
       }
-      #right {
+      .container {
          flex: 50%;
       }
-      .container {
+      #rightPane {
          flex: 50%;
       }
       #editinput {
@@ -35,33 +35,17 @@ add-editor: js-native[]{
       }
    `
 
-   var s2 = document.createElement('div')
-   s2.id = "right"
-   s2.type
+   var rightPane = document.createElement('div')
+   rightPane.id = "rightPane"
 
-   var form = document.createElement( 'form')
-   form.id = "editor"
-   
-   var input = document.createElement( 'textarea')
-   input.id = "input"
-   
-   var editdiv = document.createElement( 'div')
-   editdiv.contenteditable = "true"
-   editdiv.innerHTML = "editme"
-   editdiv.id = "editfield"
+   rightPane.onkeydown = function( e) {
+      e.stopPropagation()
+   }
+   rightPane.addEventListener( 'keypress', function( e) {
+      e.stopPropagation()
+   })
 
-   var button = document.createElement( 'button')
-   button.id = "do"
-   button.name = "do"
-   button.text = "DO"
-   button.type = "button"
-   button.innerText = "DO"
-
-   //form.appendChild( input)
-   //s2.appendChild( editdiv)
-   //s2.appendChild( button)
-
-   s2.innerHTML = `
+   rightPane.innerHTML = `
    <!--<div contenteditable="true" id="editinput">Edit me</div>-->
    <textarea id="editinput">Edit me</textarea>
    <button type="button" id="buttonedit">Edit</button>
@@ -69,52 +53,39 @@ add-editor: js-native[]{
    <button type="button" id="buttondo">DO</button>
    `
 
-   body.appendChild( s2);
+   body.appendChild( rightPane);
    document.head.appendChild( style)
+
+   var buttondo = document.getElementById( "buttondo")
+   buttondo.onclick = function( e) {
+      try {
+         reb.Value( document.getElementById( 'editinput').value)
+      } catch {
+         console.log( "Error")
+      }
+   }
 }
 
-
-
-jsDeactivateInput: js-native[] {
-    var inputDivs = document.getElementsByClassName( "input")
-    var el = inputDivs[inputDivs.length -1]
-    document.input = null
-    el.onkeydown = null
-    el.contentEditable = false
-
-    // shrinks the previous input down to a
-    // minimum size that will fit its contents
-    el.style.width = 'auto'
-    el.style.height = 'auto'
-}
-
-jsedit: js-native [src [text!]]{
-   // set element text
+jsedit: js-native [
+   "Set the editor text"
+   src [text!]
+]{
    var e = document.getElementById( "editinput")
    // using a div
    // e.innerText = reb.Spell(reb.Arg("src"))
    // using a textarea
    e.value = reb.Spell(reb.Arg("src"))
 
-   // trying to get the input handler to accept the editor as input field
-   // Deactivate Input Element
-   var inputDivs = document.getElementsByClassName( "input")
-   var el = inputDivs[inputDivs.length -1]
-   document.input = null
-   el.onkeydown = null
-   el.contentEditable = false
-
-   // shrinks the previous input down to a
-   // minimum size that will fit its contents
-   el.style.width = 'auto'
-   el.style.height = 'auto'
-
-   //DeactivateInput()
-   ActivateInput( e)
-
 }
 
-edit: function [src [text! binary! url!]][
+edit: function [
+   "Convert to text / read data, and open the editor on it"
+   src [text! binary! url!]
+   /only "take input as is (better names? as-is / mold"
+][
+   if only [
+      mold src
+   ]
    if url? src [
       ; for CORS you need to access https sites
       if parse src ["http://" to end] [insert at src 5 "s"]
@@ -129,6 +100,7 @@ edit: function [src [text! binary! url!]][
       fail "unable to get text from source"
    ]
 ]
+
 
 ; For testing
 remove-editor
