92721ba6b2cc7745bb5ee79d54939c2aeb119a6f
Ingo Hohmann <mail1@2b1.de>
2019-09-19 20:15:58 +0200

    db: changing string! to text! / add todo

--- db.r
+++ db.r
@@ -1,20 +1,24 @@
 REBOL [
    Title: "localStorage Access for Rebol"
    Author: "Ingo Hohmann"
+   Todo: {
+      - should there be an automatic conversion to/from text! for other types?
+      - indexeddb
+   }
 ]
 
 db: make object! [
    set: js-native [
       "Save to localstorage"
-      key [string!]
-      value [string!]
+      key [text!]
+      value [text!]
    ]{
       localStorage.setItem(reb.Spell(reb.Arg("key")), reb.Spell(reb.Arg("value")))
    }
 
    get: js-native [
       "Get data back from localStorage"
-      key [string!]
+      key [text!]
    ]{
       var v
       if (v = localStorage.getItem(reb.Spell(reb.ArgR("k")))) {
